26209a1e19576de5fbc27ca1f8431c1c9aa4a16e
Mahmoud <mamo31st@gmail.com>
2023-08-08 11:46:23 -0700

    updates

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,10 +5,10 @@ islower = __import__('7-islower').islower
 def uppercase(str):
     """Function prints a string in uppercase."""
     for c in str:
+        ch = c
         if islower(c):
             cc = ord(c) - 32
             ch = chr(cc)
-            print(ch, end='')
-        else:
-            print(c, end='')
+
+        print(ch, end='')
     print()
