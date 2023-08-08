2867c33bd01256b345584173c85deaa1d8fd6c66
Mahmoud <mamo31st@gmail.com>
2023-08-08 10:56:50 -0700

    updates

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for c in range(100):
-    if c < 10:
-        print("0{}".format(c), end='')
-    else:
-        print("{}".format(c), end='')
     if c % 99:
-        print(', ', end="")
+        print("{:0>2}".format(c), end=', ')
+    else:
+        print("{:0>2}".format(c), end='')
