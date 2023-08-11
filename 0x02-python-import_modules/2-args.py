ac689c4c6afdd1ef4c606f923d040f8393d76ddc
Mahmoud <mamo31st@gmail.com>
2023-08-11 07:42:24 -0700

    updates

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+args = sys.argv.copy()[1:]
+len = len(args)
+args_str = "arguments:"
+
+if len == 1:
+    args_str = "argument:"
+
+if len == 0:
+    args_str = "arguments."
+
+print(f"{len} {args_str}")
+
+for i, arg in enumerate(args):
+    print(f"{i + 1}: {arg}")
