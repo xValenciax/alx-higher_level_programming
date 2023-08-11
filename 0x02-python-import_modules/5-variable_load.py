9ed31d3414cfac1d26a627c190c29d4e941ac49f
Mahmoud <mamo31st@gmail.com>
2023-08-11 08:50:49 -0700

    updates

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-import variable_load_5
 
-print("{}".format(variable_load_5.a))
+if __name__ == "__main__":
+    import variable_load_5
+
+    print("{}".format(variable_load_5.a))
