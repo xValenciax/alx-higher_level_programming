ec3adc30c0f8c15ac3c3913dc0495da39b93114f
Mahmoud <mamo31st@gmail.com>
2023-08-16 19:56:23 -0700

    updates

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# AUTHORS Selim
+
+def roman_to_int(roman_string):
+    if roman_string == None or str(type('x')) != "<class 'str'>":
+        return 0
+
+    rc = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    deuce = False
+    number = 0
+
+    for i, key in enumerate(roman_string):
+        if i + 1 < len(roman_string) and rc[roman_string[i]] < rc[roman_string[i + 1]]:
+            number += (rc[roman_string[i + 1]] - rc[roman_string[i]])
+            deuce = True
+        elif deuce:
+            deuce = False
+        else:
+            number += rc[roman_string[i]]
+
+    return number
