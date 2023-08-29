479872c58e5b7b87057084982b92fe5d43846ff4
Selim <mamo31st@gmail.com>
2023-08-29 01:37:11 +0000

    updates

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            if str(type(my_list[i])).__contains__('int'):
+                print("{:d}".format(my_list[i]), end='')
+                count += 1
+    except ValueError:
+        pass
+    else:
+        print()
+        return count
