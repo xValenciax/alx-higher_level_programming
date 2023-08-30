8635b79f3e0da768399b1c105d6d9c33b182de2b
Selim <mamo31st@gmail.com>
2023-08-30 02:02:05 +0000

    updates

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""This a Module that contains one class called Square"""
+
+
+class Square:
+    """Represents a Square, but it's empty for now"""
+
+    def __init__(self, size=0):
+        """initializes the data"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not str(type(size)).__contains__('int'):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if not self.size:
+            print()
+            return
+        for i in range(self.area()):
+            print('#', end='')
+            if (i + 1) % self.size == 0:
+                print()
