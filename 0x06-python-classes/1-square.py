40561f6370e9fd01b5d0a02784a267674b0b8fea
Selim <mamo31st@gmail.com>
2023-08-30 01:28:19 +0000

    updates

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3\
+
+"""This a Module that contains one class called Square"""
+
+
+class Square:
+    """Represents a Square, but it's empty for now"""
+
+    def __init__(self, size):
+        """initializes the data"""
+        self.__size = size
