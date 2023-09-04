2c08bcd8b2448c7175809b862e1bc4a41a13dfba
Selim <mamo31st@gmail.com>
2023-09-04 19:00:43 +0000

    updates

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""defines a rectangle class"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0) -> None:
+        self.width = width
+        self.height = height
+
+    def __str__(self) -> str:
+        if self.width == 0 or self.height == 0:
+            return ""
+
+        rect_list = []
+
+        for i in range(self.height):
+            for j in range(self.width):
+                rect_list.append('#')
+            rect_list.append('\n')
+        rect_list.pop(-1)
+        return ''.join(rect_list)
+
+    def __repr__(self) -> str:
+        """return an eval-valid string representation of the class"""
+        return 'Rectangle({}, {})'.format(self.width, self.height)
+
+    def __del__(self) -> None:
+        """Class Destructor"""
+        print('Bye rectangle...')
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
