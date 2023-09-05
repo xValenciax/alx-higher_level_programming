404c0f1980b159917e08c199a86cec3229e2ddac
Selim <mamo31st@gmail.com>
2023-09-05 18:33:03 +0000

    updates

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,9 +6,4 @@ defines LockedClass
 
 class LockedClass:
     """represents a class that only accepts first_name as an attribute"""
-
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(
-                f"'{type(self).__name__}' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+    __slots__ = ['first_name']
