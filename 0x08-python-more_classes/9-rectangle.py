98e04f0febee530662705bf4fbdb6b545807167f
Selim <mamo31st@gmail.com>
2023-09-05 01:05:23 +0000

    updates

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -64,9 +64,12 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError('rect_1 must be an instance of Rectangle')
-        if not isinstance(rect_2, Rectangle):
+        elif not isinstance(rect_2, Rectangle):
             raise TypeError('rect_2 must be an instance of Rectangle')
-        return rect_1 if Rectangle.area(rect_1) >= Rectangle.area(rect_2) else rect_2
+        if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
+            return rect_1
+        else:
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
