f71cbac3c31b3e18f3049fef38140502194bfbaf
Selim <mamo31st@gmail.com>
2023-09-11 15:44:32 +0000

    updates

--- 0x0A-python-inheritance/3-is_king_of_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""module that defines is_kind_of_class function"""
-
-
-def is_kind_of_class(obj, a_class):
-    """checks if the object is an instance of,
-    or if the object is an instance of a class that inherited from,
-    the specified class"""
-    return isinstance(obj, a_class)
