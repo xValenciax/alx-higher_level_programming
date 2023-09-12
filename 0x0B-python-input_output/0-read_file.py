cdfc74a872a6c93e88cb08169228509fe19a436e
Selim <mamo31st@gmail.com>
2023-09-12 16:42:55 +0000

    updates

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+
 """module that defines the read file function"""
 
 
 def read_file(filename=""):
+    """reads a file and prints its content"""
     content = ""
     with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
