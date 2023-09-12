bd47f09a12061e93603047217cf9d7d405aab09d
Selim <mamo31st@gmail.com>
2023-09-12 17:30:10 +0000

    updates

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-from_json_string = __import__('4-from_json_string').from_json_string
 
-s_my_list = "[1, 2, 3]"
-my_list = from_json_string(s_my_list)
-print(my_list)
-print(type(my_list))
+"""module that defines from json string function"""
+import json
 
-s_my_dict = """
-{"is_active": true, "info": {"age": 36, "average": 3.14}, 
-"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
-"""
-my_dict = from_json_string(s_my_dict)
-print(my_dict)
-print(type(my_dict))
 
-try:
-    s_my_dict = """
-    {"is_active": true, 12 }
-    """
-    my_dict = from_json_string(s_my_dict)
-    print(my_dict)
-    print(type(my_dict))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+def from_json_string(my_str):
+    """converts a JSON string into an object representation"""
+    return json.loads(my_str)
