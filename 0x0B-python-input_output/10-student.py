38ecd96533f6bd55d3c95f0a1c1d6f959d8187e0
Selim <mamo31st@gmail.com>
2023-09-13 00:06:16 +0000

    updates

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """represents a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (isinstance(attrs, list)
+                and all([isinstance(x, str) for x in attrs])):
+            new_dict = {attr: self.__dict__[attr]
+                        for attr in attrs if attr in list(self.__dict__.keys())}
+            return new_dict
+
+        return self.__dict__
