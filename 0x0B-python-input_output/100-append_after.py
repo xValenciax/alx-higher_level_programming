227196bac31252b2b89efc0fb81b514e63c0577e
Selim <mamo31st@gmail.com>
2023-09-13 01:18:04 +0000

    updates

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 """module defines append_after function"""
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """append new_string after each line in which the search_string occurs"""
     new_lines = []
     with open(filename, 'r', encoding='UTF-8') as f:
         lines = f.readlines()
