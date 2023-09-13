6c2ceeea98bce56247793a44a8d695d0dda65c65
Selim <mamo31st@gmail.com>
2023-09-13 02:11:56 +0000

    updates

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""script that prints stat of formatted input through stdin"""
 
 import sys
-import os
 
 
 def collect_stats(lines=[], status_codes={}, file_size=0):
+    available_stats = [200, 301, 400, 401, 403, 404, 405, 500]
     for line in lines:
         lines_splitted = line.split(' ')
         file_size += int(lines_splitted[-1])
-        if status_codes.get(lines_splitted[-2]):
+        if int(lines_splitted[-2]) in available_stats and status_codes.get(lines_splitted[-2]):
             status_codes[lines_splitted[-2]] += 1
         else:
             status_codes[lines_splitted[-2]] = 1
@@ -18,7 +19,7 @@ def collect_stats(lines=[], status_codes={}, file_size=0):
 def print_stats(status_codes={}, file_size=0):
     keys = list(status_codes.keys())
     sys.stdout.write(f'File size: {file_size}\n')
-    for key in keys:
+    for key in sorted(keys):
         sys.stdout.write(f'{key}: {status_codes[key]}\n')
 
 
