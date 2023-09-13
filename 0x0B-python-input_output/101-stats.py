fd7d24638b76f7917e478e0cd7bf9aed662f6c9f
Selim <mamo31st@gmail.com>
2023-09-13 02:15:01 +0000

    updates

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """script that prints stat of formatted input through stdin"""
 
-import sys
-
 
 def collect_stats(lines=[], status_codes={}, file_size=0):
+    """collects stat from stdin lines"""
     available_stats = [200, 301, 400, 401, 403, 404, 405, 500]
     for line in lines:
         lines_splitted = line.split(' ')
@@ -17,24 +16,26 @@ def collect_stats(lines=[], status_codes={}, file_size=0):
 
 
 def print_stats(status_codes={}, file_size=0):
+    """prints the collected stats in a certain format"""
     keys = list(status_codes.keys())
     sys.stdout.write(f'File size: {file_size}\n')
     for key in sorted(keys):
         sys.stdout.write(f'{key}: {status_codes[key]}\n')
 
 
-try:
-    file_size = 0
-    status_codes = {}
-    lines = []
-    while True:
-        lines.append(sys.stdin.readline())
-        if len(lines) == 10:
-            file_size = collect_stats(lines, status_codes, file_size)
-            print_stats(status_codes, file_size)
-            lines.clear()
-except KeyboardInterrupt as e:
-    pass
-
-file_size = collect_stats(lines, status_codes, file_size)
-print_stats(status_codes, file_size)
+if __name__ == '__main__':
+    import sys
+    try:
+        file_size = 0
+        status_codes = {}
+        lines = []
+        while True:
+            lines.append(sys.stdin.readline())
+            if len(lines) == 10:
+                file_size = collect_stats(lines, status_codes, file_size)
+                print_stats(status_codes, file_size)
+                lines.clear()
+    except KeyboardInterrupt as e:
+        file_size = collect_stats(lines, status_codes, file_size)
+        print_stats(status_codes, file_size)
+        raise
