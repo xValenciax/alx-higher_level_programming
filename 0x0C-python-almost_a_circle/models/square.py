52797a41fd1f5fb8120e36f4e7768ed6c8b4b92e
Selim <mamo31st@gmail.com>
2023-09-18 15:33:02 +0000

    updates

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this is a module that defines the Rectangle Class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """represents a square"""
+
+    def __init__(self, size=0, x=0, y=0, id=None):
+        """initializes an instances of the square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y, self.width)
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""module that creates unit tests for Base class"""
+
+import unittest
+from io import StringIO
+import sys
+from models.rectangle import Rectangle
+from models.base import Base
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        Rectangle._Base__nb_objects = 0
+        cls.output_buffer = StringIO()
+        sys.stdout = cls.output_buffer
+
+    @classmethod
+    def tearDownClass(cls):
+        sys.stdout = sys.__stdout__
+        cls.output_buffer.close()
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+        self.output_buffer.truncate(0)
+        self.output_buffer.seek(0)
+
+    def test_type(self):
+        square = Square(4, 2, 2)
+        self.assertTrue(isinstance(square, Base))
+        self.assertTrue(isinstance(square, Rectangle))
+        self.assertTrue(isinstance(square, Square))
+        self.assertTrue(issubclass(Square, Base))
+        self.assertTrue(issubclass(Square, Rectangle))
+        self.assertFalse(isinstance(Square, Base))
+        self.assertFalse(isinstance(Square, Rectangle))
+
+    def test_attrs(self):
+        square = Square(4, 2, 2)
+        self.assertEqual(square.id, 1)
+        square.id = 10
+        self.assertEqual(square.id, 10)
+
+        with self.assertRaises((ValueError, TypeError)):
+            square.width = -10
+            square.x = -10
+            square.y = 'hello'
+
+        print(square)
+        output = self.output_buffer.getvalue()
+        self.assertEqual(output, '[Square] (10) 2/2 - 4\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
