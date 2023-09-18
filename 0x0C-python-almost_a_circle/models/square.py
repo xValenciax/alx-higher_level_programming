88b94173abde5eee3b9d1c0cb61bc961591643d6
Selim <mamo31st@gmail.com>
2023-09-18 21:57:34 +0000

    updates

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -50,3 +50,7 @@ class Square(Rectangle):
                     self.x = value
                 elif key == 'y':
                     self.y = value
+
+    def to_dictionary(self):
+        return {'id': self.id, 'size': self.size,
+                'x': self.x, 'y': self.y}
