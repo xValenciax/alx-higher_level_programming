f9c60d7a30080861df883e9bb5b02ccf93d37cce
Selim <mamo31st@gmail.com>
2023-10-20 10:57:47 +0300

    updates

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create user user_0d_1
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL ON *.* TO 'user_0d_01'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_01@localhost;
