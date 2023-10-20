e6295df7169d83f28175ccfb5c0659dedbae0775
Selim <mamo31st@gmail.com>
2023-10-20 20:34:41 +0300

    updates

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- lists all shows
+-- and all genres linked to that show
+
+SELECT ts.title, tg.name AS "genre"
+       FROM tv_shows ts
+LEFT JOIN tv_show_genres sg
+     ON ts.id = sg.show_id
+LEFT JOIN tv_genres tg
+     ON tg.id = sg.genre_id
+ORDER BY ts.title, tg.name;
