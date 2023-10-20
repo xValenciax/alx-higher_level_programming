cf5ab8f4b0d141ee14052409caf0cd8f166b4833
Selim <mamo31st@gmail.com>
2023-10-20 18:49:59 +0300

    updates

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- lists all shows in hbtn_0d_tvshows
+
+SELECT s.title, g.id
+       FROM tv_shows s
+INNER JOIN tv_show_genres sg
+      ON s.id = sg.show_id
+INNER JOIN tv_genres g
+      ON sg.genre_id = g.id
+ORDER BY s.title, g.id;
