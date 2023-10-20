c9ea66e36d10c7bc841d03ebd396006e37e578d3
Selim <mamo31st@gmail.com>
2023-10-20 11:17:53 +0300

    updates

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- and user user_0d_2
 
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO user_0d_2@localhost;
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+GRANT SELECT ON hbtn_0d_2 TO user_0d_2@localhost;
