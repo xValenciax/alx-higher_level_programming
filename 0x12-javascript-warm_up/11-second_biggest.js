bf13c7ecb091a26409cf646b360001746f1ab8b6
Selim <mamo31st@gmail.com>
2023-11-14 05:55:49 +0200

    updates

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+args = process.argv.slice(2);
+
+if (!args)
+    console.log(0);
+
+else {
+    let max = args[0];
+    let sec_max = args[0];
+    for (let arg of args) {
+        if (arg > max)
+        {
+            sec_max = max;
+            max = arg;
+        }
+    }
+    console.log(sec_max);
+}
